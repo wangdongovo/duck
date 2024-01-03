c9e354debe15bea9c4e7fb6e6441f169c2cfc472
Martina Emerson <wangdongovo@gmail.com>
2024-01-03 10:54:23 +0800

    Initial Commit

---
+++ duck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  duck
+//
+//  Created by wd on 2024/1/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
